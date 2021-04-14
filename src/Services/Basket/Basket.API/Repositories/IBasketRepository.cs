c4276c2c728c76f2c61016d5bff0b6757a971f11
Thanh Van Vu <thanh.vu@hitachivantara.com>
2021-04-14 23:41:30 +0700

    add Basket service

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Basket.API.Entities;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string username);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart);
+        Task DeleteBasket(string username);
+    }
+}
