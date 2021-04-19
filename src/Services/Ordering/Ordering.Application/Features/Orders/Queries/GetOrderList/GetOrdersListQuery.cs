1542a4bffa0ca2aff0192ac6c57da99846568e88
Thanh Van Vu <thanh.vu@hitachivantara.com>
2021-04-19 23:53:19 +0700

    add Ordering.API

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrdersListQuery.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using MediatR;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public class GetOrdersListQuery : IRequest<List<OrdersViewModel>>
+    {
+        public string UserName { get; set; }
+
+        public GetOrdersListQuery(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
